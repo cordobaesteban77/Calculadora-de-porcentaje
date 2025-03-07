277b41b0b1566c56d029194f7fa204b678bf9938
Esteban <cordobaesteban77@gmail.com>
2025-03-07 06:03:26 -0300

    primer commit estructura con algunos estilos y la funcion en js

---
+++ app.js
@@ -0,0 +1,10 @@
+//Algoritmo:
+// Datos de entrada: numero al que le quiero calcular el porcentaje
+// Porcentaje que deseo calcular
+// Datos de salida: Porcentaje del numero ingresado
+// Procesos: Calcular el porcentaje del numero ingresado
+// mostrar el porcentaje
+// preguntar si desea calcular otro numero//
+
+
+const calcularPorcentaje = (numero, porcentaje) => (numero * porcentaje) / 100
